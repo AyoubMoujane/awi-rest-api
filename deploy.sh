0c45cce429615330cb2bd395be8b345c6d7c6ff3
AyoubMoujane <moujane.ayoub@hotmail.com>
2021-03-08 21:47:01 +0100

    Changed port 3000 -> 8080

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ DOCKER_IMAGE=ayoubmoujane/awi-rest-api
 docker pull $DOCKER_IMAGE
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
-docker run -d -p 8080:3000 $DOCKER_IMAGE
+docker run -d -p 8080:8080 $DOCKER_IMAGE
