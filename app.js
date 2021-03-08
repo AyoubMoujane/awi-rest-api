755dd50437e4190b353c16843fa8759635f0757f
AyoubMoujane <moujane.ayoub@hotmail.com>
2021-03-08 21:43:07 +0100

    test

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const mongoose = require('mongoose')
 const app = express()
 
 var corsOptions = {
-    origin: "http://localhost:3000"
-    // origin: "http://15.236.140.17:3000"
+    // origin: "http://localhost:3000"
+    origin: "http://15.236.140.17:3000"
 };
 
 app.use(cors(corsOptions))
